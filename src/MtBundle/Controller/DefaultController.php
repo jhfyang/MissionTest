78bd8c8d6e7fe464685f3638432bd20de0432d74
SB\jyang <y4ng.jh@gmail.com>
2017-06-19 01:21:52 +0200

    Finished

--- src/MtBundle/Controller/DefaultController.php
+++ src/MtBundle/Controller/DefaultController.php
@@ -8,6 +8,35 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('MtBundle:Default:index.html.twig');
+/*    	$dataFile = file_get_contents('https://api.jcdecaux.com/vls/v1/stations?contract=Paris&apiKey=d17222d29a5d856f37d0754acef991ca05ea8ff9');
+    	$data = json_decode($dataFile);
+
+
+        return $this->render('MtBundle:Default:index.html.twig', [
+        	'data' => $data,
+        ]);*/
+
+	    $dataFile = file_get_contents('https://api.jcdecaux.com/vls/v1/stations?contract=Paris&apiKey=d17222d29a5d856f37d0754acef991ca05ea8ff9');
+	    $data = json_decode($dataFile, true);
+
+
+	    $stations = [];
+	    foreach ($data as $station){
+	    	$stations[] = [
+	    		'address' => $station['address'],
+	    		'name' =>$station['name'],
+	    		'position' => $station['position'],
+		        'bike_stands' => $station['bike_stands'],
+		        'available_bike_stands' => $station['available_bike_stands'],
+		        'available_bikes' => $station['available_bikes'],
+	        ];
+	    }
+
+
+	    return $this->render('MtBundle:Default:index.html.twig', [
+		    'stations'  => $stations,
+	        'data'      => $data
+	    ]);
+
     }
 }
